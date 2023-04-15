fb6d3d4f7c53dc3b06c2250cdf0bf9dd775ced76
sako0 <y.m1911a1.0921@gmail.com>
2023-04-15 23:35:02 +0900

    work-flowを追加

--- app/domain/model/room.go
+++
@@ -1,9 +0,0 @@
-package model
-
-import "gorm.io/gorm"
-
-type Room struct {
-	gorm.Model
-	Name    string
-	Players []*Player
-}
--- app/domain/model/ws_message.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type WsMessage struct {
-	Type           string `json:"type"`
-	Data           string `json:"data"`
-	TargetPlayerID string `json:"targetPlayerID,omitempty"`
-}
