021488e2e05707dca47a16b2524bd10445a9d9fb
sako0 <y.m1911a1.0921@gmail.com>
2023-05-05 13:25:18 +0900

    syncを使うように修正

--- app/infra/In_memory_room_repository.go
+++ app/infra/In_memory_room_repository.go
@@ -1,9 +1,14 @@
 package infra
 
-import "github.com/sako0/minigame-space-api/app/domain/model"
+import (
+	"sync"
+
+	"github.com/sako0/minigame-space-api/app/domain/model"
+)
 
 type InMemoryRoomRepository struct {
 	rooms map[uint]*model.Room
+	mu    sync.RWMutex
 }
 
 func NewInMemoryRoomRepository() *InMemoryRoomRepository {
@@ -11,19 +16,31 @@ func NewInMemoryRoomRepository() *InMemoryRoomRepository {
 }
 
 func (r *InMemoryRoomRepository) GetRoom(roomId uint) (*model.Room, bool) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+
 	room, ok := r.rooms[roomId]
 	return room, ok
 }
 
 func (r *InMemoryRoomRepository) AddRoom(roomId uint, room *model.Room) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
 	r.rooms[roomId] = room
 }
 
 func (r *InMemoryRoomRepository) RemoveRoom(roomId uint) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
 	delete(r.rooms, roomId)
 }
 
 func (r *InMemoryRoomRepository) GetAllRooms() []*model.Room {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+
 	rooms := make([]*model.Room, 0, len(r.rooms))
 	for _, room := range r.rooms {
 		rooms = append(rooms, room)
--- app/infra/in_memory_client_repository.go
+++ app/infra/in_memory_client_repository.go
@@ -1,12 +1,15 @@
 package infra
 
 import (
+	"sync"
+
 	"github.com/sako0/minigame-space-api/app/domain/model"
 	"github.com/sako0/minigame-space-api/app/domain/repository"
 )
 
 type InMemoryClientRepository struct {
 	clients map[uint]map[string]*model.Client
+	mu      sync.RWMutex
 }
 
 func NewInMemoryClientRepository() repository.ClientRepository {
@@ -14,6 +17,9 @@ func NewInMemoryClientRepository() repository.ClientRepository {
 }
 
 func (r *InMemoryClientRepository) GetClient(userId string) (*model.Client, bool) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+
 	for _, clients := range r.clients {
 		if client, ok := clients[userId]; ok {
 			return client, true
@@ -23,6 +29,9 @@ func (r *InMemoryClientRepository) GetClient(userId string) (*model.Client, bool
 }
 
 func (r *InMemoryClientRepository) AddClient(client *model.Client) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
 	if _, ok := r.clients[client.RoomId]; !ok {
 		r.clients[client.RoomId] = make(map[string]*model.Client)
 	}
@@ -30,12 +39,18 @@ func (r *InMemoryClientRepository) AddClient(client *model.Client) {
 }
 
 func (r *InMemoryClientRepository) RemoveClient(userId string) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
 	for _, clients := range r.clients {
 		delete(clients, userId)
 	}
 }
 
 func (r *InMemoryClientRepository) GetAllClientsByRoomId(roomId uint) []*model.Client {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+
 	clients := make([]*model.Client, 0, len(r.clients[roomId]))
 	for _, client := range r.clients[roomId] {
 		clients = append(clients, client)
